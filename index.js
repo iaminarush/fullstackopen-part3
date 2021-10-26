db06f1e31e8fb8cc94020b44951dfb41dc0242b7
Patrick <patrickcheng1995@gmail.com>
2021-10-26 10:04:53 +0800

    3.3

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
-  console.log(person ? true : false);
   if (person) {
     response.json(person);
   } else {
