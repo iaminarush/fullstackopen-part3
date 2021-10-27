4d5db07fdf367d424c79fcf06b1fda518ae3312e
Patrick <patrickcheng1995@gmail.com>
2021-10-27 16:16:16 +0800

    3.22

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+};
