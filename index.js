556f10171b1ac6698fa93c028fa02acdd99d1046
Patrick <patrickcheng1995@gmail.com>
2021-10-27 11:21:57 +0800

    3.15

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const app = express();
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
-app.use(express.static("build"));
 
 let persons = [
   {
@@ -49,14 +49,23 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+  // const id = Number(request.params.id);
+  // const person = persons.find((person) => person.id === id);
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
 });
 
 app.post("/api/persons", (request, response) => {
@@ -88,11 +97,17 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 
-  response.status(204).end();
+  // const id = Number(request.params.id);
+  // persons = persons.filter((person) => person.id !== id);
+
+  // response.status(204).end();
 });
 
 app.get("/info", (request, response) => {
@@ -101,6 +116,22 @@ app.get("/info", (request, response) => {
   <p>${Date().toString()}</p>
   `);
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if ((error.name = "CastError")) {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
